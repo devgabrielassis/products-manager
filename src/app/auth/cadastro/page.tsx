c6b29f6a7a04f424a0921e796169ed9e8eb6ccca
Gabriel Assis <43077215+bielworm@users.noreply.github.com>
2025-06-01 14:06:53 -0300

    feat: adicionar botão de voltar

--- src/app/auth/cadastro/page.tsx
+++ src/app/auth/cadastro/page.tsx
@@ -1,23 +1,32 @@
-'use client'
+"use client";
 import FormRegister from "@/components/Forms/FormRegister";
+import { BackIcon } from "@/components/Icons/BackIcon";
 import ModalRegisterSuccess from "@/components/Modals/ModalRegisterSuccess";
-import { useDisclosure } from "@heroui/react";
+import { Button, useDisclosure } from "@heroui/react";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 export default function Page() {
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const router = useRouter();
+  const goBack = () => router.back();
   return (
     <main className="min-h-screen content-center">
       <div className="bg-black border max-w-[88vw] p-6 md:max-w-auto md:p-8 rounded-xl w-fit mx-auto">
+        <Button
+          onPress={goBack}
+          color="default"
+          startContent={<BackIcon />}
+          variant="bordered"
+        >
+          Volar
+        </Button>
         <h2 className="text-center font-bold text-xl text-white mb-2">
           Cadastre-se
         </h2>
         <FormRegister onOpen={onOpen} />
-        <ModalRegisterSuccess
-          isOpen={isOpen}
-          onOpenChange={onOpenChange}
-        />
-    </div>
+        <ModalRegisterSuccess isOpen={isOpen} onOpenChange={onOpenChange} />
+      </div>
     </main>
   );
 }
--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,10 +1,24 @@
+"use client";
 import FormLogin from "@/components/Forms/FormLogin";
+import { BackIcon } from "@/components/Icons/BackIcon";
+import { Button } from "@heroui/react";
+import { useRouter } from "next/navigation";
 import React from "react";
 
-export default function page() {
+export default function Page() {
+  const router = useRouter();
+  const goBack = () => router.back();
   return (
     <main className="min-h-screen content-center">
-      <div className="bg-black border max-w-[88vw] p-6 md:max-w-auto md:p-8 rounded-xl w-fit mx-auto">
+      <div className="bg-black border min-w-[24vw] max-w-[88vw] p-6 md:max-w-auto md:p-8 rounded-xl w-fit mx-auto">
+        <Button
+          onPress={goBack}
+          color="default"
+          startContent={<BackIcon />}
+          variant="bordered"
+        >
+          Volar
+        </Button>
         <h2 className="text-center font-bold text-xl text-white mb-2">
           Faça Login
         </h2>
